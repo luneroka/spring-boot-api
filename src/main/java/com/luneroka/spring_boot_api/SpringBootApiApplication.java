53ada00d31df1f24eab8a26be35758f15a02a3d2
Luneroka <luneroka@gmail.com>
2025-08-02 19:42:23 +0200

    Create SoftwareEngineer class

--- src/main/java/com/luneroka/spring_boot_api/SpringBootApiApplication.java
+++ src/main/java/com/luneroka/spring_boot_api/SpringBootApiApplication.java
@@ -2,12 +2,19 @@ package com.luneroka.spring_boot_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
 
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
+	}
 }
